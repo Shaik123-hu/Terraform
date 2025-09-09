83cadd5751f87c57c109b0672b4f3e3c2167b439
Shaik123-hu <skyunus841@gmail.com>
2025-09-09 18:41:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "filename" {
+default ="pets.txt"
   description = "The name of the file to create"
   type        = string
 }
 
 variable "content" {
+default ="I love cats"
   description = "The content to put inside the file"
   type        = string
 }
