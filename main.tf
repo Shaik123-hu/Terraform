aaeefb359b73d4441b6e4846c3ccb2a723e76a5c
Shaik123-hu <skyunus841@gmail.com>
2025-09-11 17:12:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,21 @@
 terraform {
   required_providers {
     local = {
-      source = "hashicorp/local"
+      source  = "hashicorp/local"
       version = "2.3.0"
     }
   }
 }
 
+variable "filename" {
+  type    = list(string)
+  default = ["file1.txt", "file2.txt", "file3.txt"]
+}
+
 resource "local_file" "pet" {
-filename = each.value
-for_each = toset(var.filename)
-content = "Hello Terraform"
+  for_each = toset(var.filename)
+  filename = each.value
+  content  = "Hello Terraform"
+}
+
 
