8f27bee40931af0c9776ae299bf0315ff1e44985
Shaik123-hu <skyunus841@gmail.com>
2025-09-11 16:38:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "2.3.0"
+    }
+  }
+}
+
 resource "local_file" "pet" {
 filename = each.value
-for_each = var.filename
+for_each = toset(var.filename)
 content = "Hello Terraform"
-}
+
